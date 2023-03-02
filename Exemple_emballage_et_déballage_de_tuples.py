b558b9da298d60faad38dd72015f5cad7019d09a
ThibautMol <thibautm@hotmail.fr>
2023-03-02 20:37:28 +0100

    update files

---
+++ Exemple_emballage_et_déballage_de_tuples.py
@@ -0,0 +1,14 @@
+
+"""emballage et déballage de tuples"""
+
+tuple=2,3,5
+x,y,z = tuple
+print("x=",x," ","y=",y," ","z=",z)
+
+
+#permutation
+
+print("permutation:")
+
+x,y,z=z,x,y
+print("x=",x," ","y=",y," ","z=",z)
